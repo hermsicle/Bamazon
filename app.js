9464ff4305629d7370f3e32c28d4360327586cfe
Herman Liu <hermanliu95@gmail.com>
2019-12-11 14:48:51 -0800

    second commit, added the mysql table

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello wolrd')
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '7upisyummY',
+    database: 'bamazon'
+})
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log(`you are connected to ID : ${connection.threadId}`);
+    showTable();
+    connection.end();
+})
+
+showTable = () => {
+    connection.query('SELECT * FROM products', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    })
+}
