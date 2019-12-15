b726f34b45c3d0c8b7ede2001462a808e8a5cd7b
Herman Liu <hermanliu95@gmail.com>
2019-12-14 16:54:13 -0800

    added a README

--- app.js
+++ app.js
@@ -42,31 +42,24 @@ startApp = () => {
                 inquirer.prompt({
                     type: 'input',
                     message: 'How many units would you like to purchase of this product Id?',
-                    name: 'userBuy'
+                    name: 'quantity'
                 }).then(answer2 => {
-                    let selectedQuantity = answer2.userBuy;
-                    let stockQuantity = res[0].stock_quantity;
-                    //console.log(res[0].stock_quantity)
-                    //console.log(selectedQuantity);
-                    //console.log(res[0].stock_quantity);
-                    if (selectedQuantity > stockQuantity) {
-                        console.log("Our apologies, we only have " + res[0].stock_quantity + " left in our inventory");
+                    console.log(res[0].stock_quantity)
+                    let quantity = answer2.quantity;
+                    if (quantity > res[0].stock_quantity) {
+                        console.log(
+                            "Our apologies, we only have " +
+                            res[0].stock_quantity +
+                            " left in our inventory"
+                        );
                         startApp();
-                    }
-                    else {
-
+                    } else {
                         console.log(res[0].product_name + " purchased");
-                        console.log(selectedQuantity + " purchased for the price of " + res[0].price + "each");
+                        console.log(quantity + " purchased for the price of " + res[0].price + "each");
                         console.log("Thank you for purchasing with us today");
 
-                        let newQuantity = stockQuantity - selectedQuantity;
+                        let newQuantity = quantity - res[0].stock_quantity;
                         console.log(newQuantity);
-                        // function updateStock(stock, id) {
-                        //     connection.query("UPDATE products SET ? WHERE ??", [{ stock_quantity: stock }, { id: id }], err => {
-                        //         if (err) throw err;
-                        //     })
-                        // }
-                        // updateStock()
                         connection.end();
                     }
                 })
